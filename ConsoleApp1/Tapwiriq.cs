a138ddae10eaff471967c1ba46336c0cabff1c63
Fahmin <fehmin200296@gmail.com>
2026-03-28 00:12:45 -0700

    necenci cehddi bilmirem

---
+++ ConsoleApp1/Tapwiriq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Tapwiriq
+    {
+   public static void profile()
+        {
+            Console.WriteLine("adinizi daxil edin");
+            string ad = Console.ReadLine(); 
+
+            Console.WriteLine("soyadinizi daxil edin");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("yasinizi daxil edin");
+            string yas = Console.ReadLine();
+
+            int yas1 = Convert.ToInt32(yas);
+            
+            Console.WriteLine($"/n---Isdifadeci karti---"); 
+            Console.WriteLine($"Ad: {ad}");
+            Console.WriteLine($"Soyad: {soyad}");
+            Console.WriteLine($"Yas: {yas1}");
+
+
+
+
+        }
+
+    }
+}
